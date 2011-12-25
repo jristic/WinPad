d99239f8bd7551d4f39f828fde4ff5b29458102e
Jovan Ristic <jristic42@gmail.com>
2011-12-25 02:20:59 -0800

    forgot to stage changes to source file before last commit

--- WinPad/WinPad/WinPad.cs
+++ WinPad/WinPad/WinPad.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 using WindowsInput;
+using WindowsInput.Native;
 
 namespace WinPad
 {
@@ -44,15 +45,53 @@ namespace WinPad
 
 		protected override void Update(GameTime gameTime)
 		{
-			updateGamePad();
+			updateGamePadState();
 
-			// Apply cursor translation
+
+			// Apply cursor translation 
 			int cursorTranslateX = (int)(gameTime.ElapsedGameTime.Milliseconds * currentGamePadState.ThumbSticks.Left.X);
 			int cursorTranslateY = (int)(gameTime.ElapsedGameTime.Milliseconds * currentGamePadState.ThumbSticks.Left.Y * -1);
 			inputSim.Mouse.MoveMouseBy(cursorTranslateX, cursorTranslateY);
 
+
 			// Simulate LMB-click if A is pressed
+			if (currentGamePadState.Buttons.A == ButtonState.Pressed && previousGamePadState.Buttons.A == ButtonState.Released)
+			{
+				inputSim.Mouse.LeftButtonDown();
+			}
+			else if (currentGamePadState.Buttons.A == ButtonState.Released && previousGamePadState.Buttons.A == ButtonState.Pressed)
+			{
+				inputSim.Mouse.LeftButtonUp();
+			}
+
+			// Simulate RMB-click if X is pressed
+			if (currentGamePadState.Buttons.X == ButtonState.Pressed && previousGamePadState.Buttons.X == ButtonState.Released)
+			{
+				inputSim.Mouse.RightButtonDown();
+			}
+			else if (currentGamePadState.Buttons.X == ButtonState.Released && previousGamePadState.Buttons.X == ButtonState.Pressed)
+			{
+				inputSim.Mouse.RightButtonUp();
+			}
 			
+
+			// Simulate arrow keys for the d-pad 
+			if (currentGamePadState.DPad.Up == ButtonState.Pressed && previousGamePadState.DPad.Up == ButtonState.Released)
+			{
+				inputSim.Keyboard.KeyPress(VirtualKeyCode.UP);
+			}
+			if (currentGamePadState.DPad.Down == ButtonState.Pressed && previousGamePadState.DPad.Down == ButtonState.Released)
+			{
+				inputSim.Keyboard.KeyPress(VirtualKeyCode.DOWN);
+			}
+			if (currentGamePadState.DPad.Left == ButtonState.Pressed && previousGamePadState.DPad.Left == ButtonState.Released)
+			{
+				inputSim.Keyboard.KeyPress(VirtualKeyCode.LEFT);
+			}
+			if (currentGamePadState.DPad.Right == ButtonState.Pressed && previousGamePadState.DPad.Right == ButtonState.Released)
+			{
+				inputSim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+			}
 
 			base.Update(gameTime);
 		}
@@ -63,7 +102,7 @@ namespace WinPad
 			base.Draw(gameTime);
 		}
 
-		private void updateGamePad()
+		private void updateGamePadState()
 		{
 			previousGamePadState = currentGamePadState;
 			currentGamePadState = GamePad.GetState(PlayerIndex.One);

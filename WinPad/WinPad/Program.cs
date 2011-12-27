fc77d4778a71740d4d955855fdc0425e0bcae9b0
Jovan Ristic <jristic42@gmail.com>
2011-12-26 22:23:31 -0800

    made it so only one instance of the program can be running

--- WinPad/WinPad/Program.cs
+++ WinPad/WinPad/Program.cs
@@ -1,21 +1,30 @@
 using System;
+using System.Threading;
 
 namespace WinPad
 {
 #if WINDOWS || XBOX
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
-        {
-            using (WinPad prog = new WinPad())
-            {
-                prog.Run();
-            }
-        }
-    }
+	static class Program
+	{
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			bool createdNew = true;
+			using (Mutex mutex = new Mutex(true, "jristic.WinPad", out createdNew))
+			{
+				if (createdNew)
+				{
+					using (WinPad prog = new WinPad())
+					{
+						prog.Run();
+					}
+				}
+			}
+		}
+	}
 #endif
 }
 
